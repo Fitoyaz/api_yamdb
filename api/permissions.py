fcfc19e194310b0145ae14d30dca2dd96bb7f127
Виталий Ковалев <ilovetulich@gmail.com>
2021-06-04 20:20:52 +0500

    v.0.42

--- api/permissions.py
+++ api/permissions.py
@@ -12,22 +12,23 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.role == 'admin' or request.user.is_superuser:
-            return True
-        else:
-            return False
+        if request.user.is_authenticated:
+            if request.user.role == 'admin' or request.user.is_superuser:
+                return True
+        return False
 
 
 class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        if (
-            request.user.role == 'moderator'
-            or request.user.role == 'admin'
-            or request.user.is_superuser
-        ):
-            return True
-        else:
-            return False
+        if request.user.is_authenticated:
+            if (
+                request.user.role == 'moderator'
+                or request.user.role == 'admin'
+                or request.user.is_superuser
+            ):
+                return True
+        return False
+
 
 
 class ReviewOwnerPermission(permissions.BasePermission):
